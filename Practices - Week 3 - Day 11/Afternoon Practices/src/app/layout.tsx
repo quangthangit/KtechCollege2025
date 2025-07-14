8bdbadb92c359b18eadc8164c57e5ee81870fc39
quangthangit <thangtq.it@gmail.com>
2025-07-15 05:50:45 +0700

    Practices - Week 3 - Day 11 - 14-07-2025

--- Practices - Week 3 - Day 11/Afternoon Practices/src/app/layout.tsx
+++ Practices - Week 3 - Day 11/Afternoon Practices/src/app/layout.tsx
@@ -1,19 +1,22 @@
-import Header from "@/components/Header";
-import "./globals.css";
-import { ReactNode } from "react";
-import Footer from "@/components/Footer";
+// app/layout.tsx or app/layout.js
+import Header from '@/components/Header';
+import './globals.css';
+import { ReactNode } from 'react';
+import Footer from '@/components/Footer';
 
 export const metadata = {
-  title: "My App",
-  description: "My App Description",
+  title: 'My App',
+  description: 'Build modern apps with ease.',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <Header />
-      <body>{children}</body>
-      <Footer />
-    </>
+    <html lang="en">
+      <Header/>
+      <body>
+        {children}
+      </body>
+      <Footer/>
+    </html>
   );
 }
