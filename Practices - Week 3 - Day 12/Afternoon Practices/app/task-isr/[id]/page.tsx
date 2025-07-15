30230a83a43203ff6e3172dbdf0de672f73f50a7
quangthangit <thangtq.it@gmail.com>
2025-07-15 15:13:27 +0700

    Practices - Week 3 - Day 12 - 15-07-2025

--- Practices - Week 3 - Day 12/Afternoon Practices/app/task-isr/[id]/page.tsx
+++ Practices - Week 3 - Day 12/Afternoon Practices/app/task-isr/[id]/page.tsx
@@ -1,6 +1,27 @@
-import IncrementalStaticRegeneration from "@/app/components/IncrementalStaticRegeneration";
+import { TaskTypes } from "@/app/types/TaskType";
+import { Task } from "@/app/components/Task";
 
-export default function Page({ params }: { params: { id: string } }) {
+export const revalidate = 10;
+const apiUrl = "https://server.aptech.io";
+export default async function Page({ params }: { params: { id: string } }) {
+  const res = await fetch(`${apiUrl}/workspaces/tasks/${params.id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ0dW5nbnRAc29mdGVjaC52biIsImVtYWlsIjoidHVuZ250QHNvZnRlY2gudm4iLCJzdWIiOjEsImFwcGxpY2F0aW9uIjoiT25saW5lIFNob3AgLSBBUEkiLCJyb2xlcyI6W3siaWQiOjEsIm5hbWUiOiJBZG1pbmlzdHJhdG9ycyJ9LHsiaWQiOjIsIm5hbWUiOiJNYW5hZ2VycyJ9XSwiaWF0IjoxNzUyNTYyMjQwLCJleHAiOjE3ODQxMTk4NDB9.uXNclD4lUgtTvF5iIExr-DLcxTugU4ZysSh1gzxmG5I`,
+    },
+    next: { revalidate },
+  });
+
+  if (!res.ok) {
+    return (
+      <div className="p-6 text-red-500">
+        ❌ Failed to load task with ID: {params.id}
+      </div>
+    );
+  }
+
+  const task: TaskTypes = await res.json();
+
   return (
     <div>
       <div className="min-h-screen flex flex-col bg-gray-100 transition-colors duration-300">
@@ -8,7 +29,12 @@ export default function Page({ params }: { params: { id: string } }) {
           <div className="text-left font-bold text-2xl text-blue-700 mb-4">
             List task manager
           </div>
-          <IncrementalStaticRegeneration params={params} />
+          <section className="p-6">
+            <h2 className="text-2xl font-bold mb-4">Tasks (ISR)</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+              <Task todo={task} />
+            </div>
+          </section>
         </div>
       </div>
 
